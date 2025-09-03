c8003fce205bd64ebf1abd9097400896ee90038b
Цеунов Матвей <matvey1996007sport@gmail.com>
2025-09-03 18:15:08 +0700

    три кавычки

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "<!doctype html>" \
-        "<html>" \
-        "   <body>" \
-        "       <hl>web-сервер на flask</h1>" \
-        "   </body>" \
-        "<html>"
+    return """<!doctype html> 
+        <html> 
+           <body>
+               <hl>web-сервер на flask</h1>
+           </body> 
+        <html>"""
