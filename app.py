29f1999ccadfa59c8c96a7ab9fdf14a8b3159aa3
Цеунов Матвей <matvey1996007sport@gmail.com>
2025-09-30 21:19:58 +0700

    цикл в шаблоне

--- app.py
+++ app.py
@@ -460,7 +460,14 @@ def example():
     number_lab = '2'
     group = 'ФБИ-31'
     course = '3'
-    return render_template('example.html', name=name, number_lab=number_lab, group=group, course=course)
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},    
+        {'name': 'апельсины', 'price': 80},    
+        {'name': 'мандарины', 'price': 95},    
+        {'name': 'манго', 'price': 321}        
+    ]
+    return render_template('example.html', name=name, number_lab=number_lab, group=group, course=course, fruits=fruits)
     
 
 
