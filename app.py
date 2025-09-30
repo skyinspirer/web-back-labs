59c6649b36b887054034b87f070a95c152bbca72
Цеунов Матвей <matvey1996007sport@gmail.com>
2025-09-30 21:01:44 +0700

    имя студента

--- app.py
+++ app.py
@@ -456,6 +456,7 @@ def add_flower (name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Цеунов Матвей Евгеньевич'
+    return render_template('example.html', name=name)
 
 
