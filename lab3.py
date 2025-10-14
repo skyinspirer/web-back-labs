3cdcb1b54080891e85d913d39826247d132299fb
Цеунов Матвей <matvey1996007sport@gmail.com>
2025-10-15 01:30:30 +0700

    отчистка куков

--- lab3.py
+++ lab3.py
@@ -148,3 +148,14 @@ def ticket_result():
                          departure=request.args.get('departure'),
                          destination=request.args.get('destination'),
                          travel_date=request.args.get('travel_date'))
+
+
+
+@lab3.route('/lab3/settings_clear')
+def settings_clear():
+    resp = make_response(redirect('/lab3/settings'))
+    resp.delete_cookie('color')
+    resp.delete_cookie('bg_color')
+    resp.delete_cookie('font_size')
+    resp.delete_cookie('line_height')
+    return resp
