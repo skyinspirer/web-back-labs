609bfd0fe142927a42ef474847f5ae96701da6e4
Цеунов Матвей <matvey1996007sport@gmail.com>
2025-11-11 22:12:32 +0700

    Правки

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, url_for, request, redirect, abort, render_template, session, os
+from flask import Flask, url_for, request, redirect, abort, render_template, session
+import os 
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
