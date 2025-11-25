1552e33e5b4d6bdd37fa7256c23f8f6bf336cdb2
Студент Факультета Бизнеса <student@FB.LOCAL>
2025-11-25 10:24:11 +0700

    бронирование офисов

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
-from lab5 import lab6
+from lab6 import lab6
 import datetime
 
 app = Flask(__name__)
--- lab6.py
+++ lab6.py
@@ -1,13 +1,65 @@
-from flask import Blueprint, url_for, request, redirect, abort, render_template, make_response, session, current_app, flash
+from flask import Blueprint, url_for, request, redirect, abort, render_template, make_response, session, current_app
 import datetime
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
-from os import path 
+from os import path
 
 lab6 = Blueprint('lab6', __name__)
+
+offices = []
+for i in range(1, 11):
+    offices.append({"number": i, "tenant": ""})
+
 
 @lab6.route("/lab6/")
 def main():
     return render_template('lab6/lab6.html')
+
+
+@lab6.route('/lab6/json-rpc-api/', methods = ['POST'])
+def api():
+    data = request.json
+    id = data['id']
+    if data['method'] == 'info':
+        return {
+            'jsonrpc': '2.0',
+            'result': offices,
+            'id': id
+        }
+    
+    login = session.get('login')
+    if not login:
+        return {
+            'jsonrpc': '2.0',
+            'error': {
+                'code': 1,
+                'message': 'Unauthorized'
+            },
+            'id': id
+        }
+    
+    elif data['method'] == 'booking':
+        office_number = data['params']
+        for office in offices:
+            if office['number'] == office_number:
+                if office['tenant'] != '':
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 2,
+                            'message': 'Already booked'
+                        },
+                        'id': id 
+                    }
+
+                office['tenant'] = login
+                return {
+                    'jsonrpc': '2.0',
+                    'error': {
+                        'code': -32601,
+                        'message': 'Method not found'
+                    },
+                    'id': id
+                }
