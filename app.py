a093ff235dfa835223998831573db98589c721c3
Студент Факультета Бизнеса <student@FB.LOCAL>
2025-12-10 16:18:19 +0700

    заготовка лабораторная работа 8

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 import datetime
 
 app = Flask(__name__)
@@ -21,7 +22,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
-
+app.register_blueprint(lab8)
 
 
 @app.route("/index")
@@ -40,6 +41,7 @@ def index():
                     <div><a href="/lab5">Лабораторная работа №5</a></div>
                     <div><a href="/lab6">Лабораторная работа №6</a></div>
                     <div><a href="/lab7">Лабораторная работа №7</a></div>
+                    <div><a href="/lab8">Лабораторная работа №8</a></div>
                 <footer>Цеунов Матвей Евгеньевич, ФБИ-31, 3 курс, 2025</footer>
           </body>
         </html>'''
@@ -60,6 +62,7 @@ def a():
                     <div><a href="/lab5">Лабораторная работа №5</a></div>
                     <div><a href="/lab6">Лабораторная работа №6</a></div>
                     <div><a href="/lab7">Лабораторная работа №7</a></div>
+                    <div><a href="/lab8">Лабораторная работа №8</a></div>
                 <footer>Цеунов Матвей Евгеньевич, ФБИ-31, 3 курс, 2025</footer>
           </body>
         </html>'''
