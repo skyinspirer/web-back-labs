938828e8d2235306326d495d51895f02f9d8405e
Цеунов Матвей <matvey1996007sport@gmail.com>
2025-12-23 22:22:06 +0700

    доп задание

--- db/models.py
+++ db/models.py
@@ -1,13 +1,11 @@
 from . import db
 from flask_login import UserMixin
 
-
 class users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     login = db.Column(db.String(30), nullable = False, unique = True)
     password = db.Column(db.String(162), nullable = False)
 
-    
 class articles(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     login_id = db.Column(db.Integer, db.ForeignKey('users.id'))
--- lab8.py
+++ lab8.py
@@ -7,6 +7,7 @@ from os import path
 from db import db
 from db.models import users, articles
 from flask_login import login_user, login_required, current_user, logout_user
+from sqlalchemy import or_
 
 
 lab8 = Blueprint('lab8', __name__)
@@ -16,7 +17,7 @@ def lab():
     return render_template('lab8/lab8.html')
 
 
-@lab8.route('/lab8/register/', methods = ['GET', 'POST'])
+@lab8.route('/lab8/register/', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
         return render_template('lab8/register.html')
@@ -30,19 +31,21 @@ def register():
     if not password_form:
         return render_template('lab8/register.html', error='Пароль не должен быть пустым')
 
-    login_exists = users.query.filter_by(login = login_form).first()
+    login_exists = users.query.filter_by(login=login_form).first()
     if login_exists:
-        return render_template('lab8/register.html', error = 'Такой пользователь уже существует')
+        return render_template('lab8/register.html', error='Такой пользователь уже существует')
     
     password_hash = generate_password_hash(password_form)
-    new_user = users(login = login_form, password = password_hash)
+    new_user = users(login=login_form, password=password_hash)
 
     db.session.add(new_user)
     db.session.commit()
+
+    login_user(new_user, remember=False)
     return redirect('/lab8/')
 
 
-@lab8.route('/lab8/login', methods = ['GET', 'POST'])
+@lab8.route('/lab8/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('lab8/login.html')
@@ -50,26 +53,41 @@ def login():
     login_form = request.form.get('login')
     password_form = request.form.get('password')
 
+    remember_me = request.form.get('remember') == 'yes'
+
     if not login_form:
-        return render_template('lab8/register.html', error='Имя пользователя не должно быть пустым')
+        return render_template('lab8/login.html', error='Имя пользователя не должно быть пустым')
     
     if not password_form:
-        return render_template('lab8/register.html', error='Пароль не должен быть пустым')
+        return render_template('lab8/login.html', error='Пароль не должен быть пустым')
 
-    user = users.query.filter_by(login = login_form).first()
+    user = users.query.filter_by(login=login_form).first()
 
     if user:
         if check_password_hash(user.password, password_form):
-            login_user(user, remember=False)
+            login_user(user, remember=remember_me)
             return redirect('/lab8/')
         
-        return render_template('/lab8/login.html', error = 'Ошибка входа: логин и/или пароль неверны')
+        return render_template('lab8/login.html', error='Ошибка входа: логин и/или пароль неверны')
     
 
 @lab8.route('/lab8/articles')
-@login_required
 def article_list():
-    return 'список статей'
+    public_articles = articles.query.filter_by(is_public=True).all()
+    
+    if current_user.is_authenticated:
+        user_articles = articles.query.filter_by(login_id=current_user.id).all()
+        all_articles = []
+        article_ids = set()
+        
+        for article in public_articles + user_articles:
+            if article.id not in article_ids:
+                article_ids.add(article.id)
+                all_articles.append(article)
+    else:
+        all_articles = public_articles
+    
+    return render_template('lab8/articles.html', articles=all_articles)
 
 
 @lab8.route('/lab8/logout')
@@ -77,3 +95,127 @@ def article_list():
 def logout():
     logout_user()
     return redirect('/lab8/')
+
+
+@lab8.route('/lab8/create', methods=['GET', 'POST'])
+@login_required
+def create_article():
+    if request.method == 'GET':
+        return render_template('lab8/create.html')
+    
+    title = request.form.get('title', '').strip()
+    article_text = request.form.get('text', '').strip()
+    is_public = request.form.get('is_public') == 'on'
+    is_favorite = request.form.get('is_favorite') == 'on'
+    
+    if not title:
+        return render_template('lab8/create.html', error='Заголовок не может быть пустым')
+    
+    if not article_text:
+        return render_template('lab8/create.html', error='Текст статьи не может быть пустым')
+    
+    new_article = articles(
+        title=title,
+        article_text=article_text,
+        is_favorite=is_favorite,
+        is_public=is_public,
+        likes=0,
+        login_id=current_user.id
+    )
+    
+    db.session.add(new_article)
+    db.session.commit()
+    
+    return redirect('/lab8/articles')
+
+
+@lab8.route('/lab8/edit/<int:article_id>', methods=['GET', 'POST'])
+@login_required
+def edit_article(article_id):
+    article = articles.query.filter_by(id=article_id, login_id=current_user.id).first()
+    
+    if not article:
+        abort(404)
+    
+    if request.method == 'GET':
+        return render_template('lab8/edit.html', article=article)
+    
+    title = request.form.get('title', '').strip()
+    article_text = request.form.get('text', '').strip()
+    is_public = request.form.get('is_public') == 'on'
+    is_favorite = request.form.get('is_favorite') == 'on'
+    
+    if not title:
+        return render_template('lab8/edit.html', article=article, error='Заголовок не может быть пустым')
+    
+    if not article_text:
+        return render_template('lab8/edit.html', article=article, error='Текст статьи не может быть пустым')
+
+    article.title = title
+    article.article_text = article_text
+    article.is_public = is_public
+    article.is_favorite = is_favorite
+    
+    db.session.commit()
+    
+    return redirect('/lab8/articles')
+
+
+@lab8.route('/lab8/delete/<int:article_id>')
+@login_required
+def delete_article(article_id):
+    article = articles.query.filter_by(id=article_id, login_id=current_user.id).first()
+    
+    if not article:
+        abort(404)
+    
+    db.session.delete(article)
+    db.session.commit()
+    
+    return redirect('/lab8/articles')
+
+
+@lab8.route('/lab8/search', methods=['GET', 'POST'])
+def search_articles():
+    if request.method == 'GET':
+        return render_template('lab8/search.html')
+
+    search_query = request.form.get('query', '').strip()
+    
+    if not search_query:
+        return render_template('lab8/search.html', error='Введите поисковый запрос')
+    
+    search_pattern = f"%{search_query}%"
+    
+    if current_user.is_authenticated:
+        user_articles = articles.query.filter(
+            articles.login_id == current_user.id,
+            or_(
+                articles.title.ilike(search_pattern),
+                articles.article_text.ilike(search_pattern)
+            )
+        ).all()
+        
+        public_articles = articles.query.filter(
+            articles.is_public == True,
+            articles.login_id != current_user.id,
+            or_(
+                articles.title.ilike(search_pattern),
+                articles.article_text.ilike(search_pattern)
+            )
+        ).all()
+        
+        articles_found = user_articles + public_articles
+    else:
+        articles_found = articles.query.filter(
+            articles.is_public == True,
+            or_(
+                articles.title.ilike(search_pattern),
+                articles.article_text.ilike(search_pattern)
+            )
+        ).all()
+    
+    return render_template('lab8/search.html', 
+                         articles=articles_found, 
+                         search_query=search_query,
+                         count=len(articles_found))
