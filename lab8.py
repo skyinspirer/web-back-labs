620ddad59a9934ffc200e4955a23262a6b914c6f
Цеунов Матвей <matvey1996007sport@gmail.com>
2025-12-23 20:17:03 +0700

    заготовка к 8

--- lab8.py
+++ lab8.py
@@ -1,34 +1,11 @@
-from flask import Blueprint, url_for, request, redirect, abort, render_template, make_response, session, current_app
-import datetime
+from flask import Blueprint, url_for, request, redirect, abort, render_template, make_response, session, current_app, jsonify
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
 from os import path
 
 lab8 = Blueprint('lab8', __name__)
 
-
 @lab8.route("/lab8/")
-def main():
+def lab():
     return render_template('lab8/lab8.html')
-
-
-@lab8.route("/lab8/register/")
-def register():
-    return render_template('lab8/register.html')
-
-
-@lab8.route("/lab8/login/")
-def login():
-    return render_template('lab8/login.html')
-
-
-@lab8.route("/lab8/articles/")
-def articles():
-    return render_template('lab8/articles.html')
-
-
-@lab8.route("/lab8/create/")
-def create():
-    return render_template('lab8/create.html')
